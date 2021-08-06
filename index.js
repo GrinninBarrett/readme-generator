330494732ab185941fefe812b4b9aea41f8d30b7
Tucker Barrett <tuckerbarrett@gmail.com>
2021-08-06 16:32:56 -0400

    Adds require from generateMarkdown.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // TODO: Include packages needed for this application
 const fs = require("fs");
 const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown.js")
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -21,9 +22,7 @@ fs.writeFile("README.md", data, err => {
 })
 
 // TODO: Create a function to initialize app
-function init() {
-    
-}
+function init() {}
 
 // Function call to initialize app
 init();
